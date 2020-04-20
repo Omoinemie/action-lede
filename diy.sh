6b49a04fa7979f9dc4116acc28a64c5d3cecfc47
ccminglove <31991443+Omoinemie@users.noreply.github.com>
2020-04-20 09:34:39 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,3 +8,6 @@
 # Modify default IP
 git clone https://github.com/fw876/helloworld package/lean
 sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+
+./scripts/feeds update -a && ./scripts/feeds install -a
