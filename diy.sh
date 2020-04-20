665471425dcda666193563e98f849c4d5dc0d55b
ccminglove <31991443+Omoinemie@users.noreply.github.com>
2020-04-20 09:37:29 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,5 +9,5 @@
 git clone https://github.com/fw876/helloworld package/lean
 sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
-
+git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
 ./scripts/feeds update -a && ./scripts/feeds install -a
